798b6df24ff0620da25832602b59aff531f516b1
Glebanister <glebmar2001@gmail.com>
2022-11-05 17:00:11 +0100

    Add Location and Entity

--- database/api.py
+++ database/api.py
@@ -3,25 +3,34 @@ import typing as t
 import common.instances as instaces
 
 
-def get_all_places(
+def get_all_entities(
     coords: instaces.Coordinates, radius: instaces.Distance
 ) -> t.List[instaces.Entity]:
     # TODO
     return []
 
 
-def get_best_places(
+def get_best_locations(
     coords: instaces.Coordinates,
     radius: instaces.Distance,
     n_max_results: int,
     division_ratio: int,
-) -> t.List[t.Tuple[instaces.Entity, instaces.EntityDescription]]:
+) -> t.List[t.Tuple[instaces.Location, instaces.LocationDescription]]:
     # TODO
     return []
 
 
-def get_overall(
+def get_overall_about_location(
     coords: instaces.Coordinates, radius: instaces.Distance
 ) -> instaces.LocationDescription:
     # TODO
     return instaces.LocationDescription()
+
+
+def get_best_entites(
+    coords: instaces.Coordinates,
+    radius: instaces.Distance,
+    n_max_results: int,
+) -> t.List[t.Tuple[instaces.Entity, instaces.EntityDescription]]:
+    # TODO
+    return []
