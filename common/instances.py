d75c2df04b012c587692f4c5469224eec693138a
rinas-ink <kotenok04in@gmail.com>
2022-11-06 10:27:53 +0100

    with evaluation

--- common/instances.py
+++ common/instances.py
@@ -46,12 +46,75 @@ class Restaraunt:
     is_vegeterian: bool
     has_vegan_options: bool
 
-    def __lt__(self, other):
-        return self.rank() < other.rank()
-
     def rank(self):
-        return self.is_bio - self.is_chain + self.is_vegan \
-               + self.is_vegeterian + self.has_vegan_options
+        bio = 1.5
+        vegan_options = 1
+        veget = 1.5
+        vegan = 4
+        chain = -0.8
+        score = 0
+        if self.is_vegan == 1:
+            score += vegan
+        if self.is_vegeterian == 1:
+            score += veget
+        if self.has_vegan_options == 1:
+            score += vegan_options
+        if self.is_bio == 1:
+            score += bio
+        if self.is_chain == 1:
+            score += chain
+        return round(max(score, 0) * 1.25, 1)
+
+    def descr(self):
+        msg = ""
+        # TO BE DEV - location - maybe?
+        # rstr.msg = "The restaurant found at the lat:  "
+        # rstr.msg += rstr.lat
+        # rstr.msg += " and long: "
+        # rstr.msg += rstr.lon
+        # CHECK rstr.vegan AND rstr.veget RETURNS
+
+        # TO BE Approved
+
+        msg += "This restaurant has a score"
+        msg += " of " + "{:.1f}/10.0"
+        if self.is_vegan == 1:
+            msg += " and it's Vegan."
+            return msg
+        elif self.is_vegeterian == 1:
+            msg += " and it's Vegetarian."
+            return msg
+
+        msg += ". It is "
+
+        # output based on is_chain value
+        if self.is_chain == 0:
+            msg += "not "
+
+        # standard output
+        msg += "part of a chain and it "
+
+        # output based on is_bio value
+        if self.is_bio == 1:
+            msg += "offers "
+        else:
+            msg += "doesn't offer "
+
+        # standard output
+        msg += "bio and organic products. "
+        msg += "However, the restaurant "
+
+        # output based on is_vegan option value
+        if self.has_vegan_options == 1:
+            msg += "offers vegan dishes."
+        else:
+            msg += "does not offer vegan dishes."
+
+        return msg
+
+
+def compare_restaurants(a, b):
+    return a.rank() - b.rank()
 
 
 @dataclass
@@ -81,7 +144,7 @@ class Location:
 
 @dataclass
 class EntityDescription:
-    desc: str = "Entity score is: "
+    desc: str = "Entity score is: {:.1f}"
     score: float = 0
 
     def __str__(self):
@@ -90,17 +153,15 @@ class EntityDescription:
     # TODO: Add common information about place - bus stop, store, ... (as strings)
 
 
-
 @dataclass
 class LocationDescription:
-    desc: str = "Location score is: {}"
+    desc: str = "Location score is: {:.1f}"
     score: float = 0
 
     def __str__(self):
         return self.desc.format(self.score)
 
     # TODO: Add common information about location (city, ) (as strings)
-
 
 
 @dataclass
--- database/api.py
+++ database/api.py
@@ -1,5 +1,6 @@
 import typing as t
 import csv
+from functools import cmp_to_key
 
 from haversine import inverse_haversine, Direction, haversine
 from dataclasses import dataclass
@@ -22,32 +23,31 @@ def construct_with_coords(
     with open(filename, newline='') as f:
         reader = csv.reader(f, delimiter=',', quotechar='"')
         for row in reader:
-            flags = [bool(i) for i in row[3:]]
+            flags = [bool(int(i)) for i in row[3:] if len(i) > 0]
             coords = instaces.Coordinates(float(row[1]), float(row[2]))
+            other = []
             id = int(row[0])
             if instance is instaces.RentalPoint:
-                other = instaces.RentalKind.BIKE
+                other.append(instaces.RentalKind.BIKE)
             elif instance is instaces.TransportStop:
-                other = instaces.TransportKind.BUS
-            if len(flags) == 0:
-                res.append(instance(coords, id, *(other,)))
-            else:
-                id = int(row[0])
-                res.append(instance(coords, id, *flags))
+                other.append(instaces.TransportKind.BUS)
+            res.append(instance(coords, id, *tuple(flags), *tuple(other)))
     return res
 
 
 class DatabaseApi:
     data_paths: t.Dict[str, Path] = {"RentalPoints": Path("tables") / "RentalPoints.csv",
-                  "Restaurants": Path("tables") / "Restaurants.csv",
-                  "TransportStops": Path("tables") / "TransportStops.csv"}
+                                     "Restaurants": Path("tables") / "Restaurants.csv",
+                                     "TransportStops": Path("tables") / "TransportStops.csv"}
     tables = {}
 
     def __init__(self, root: Path):
-        self.tables["RentalPoints"] = construct_with_coords(root / self.data_paths["RentalPoints"], instaces.RentalPoint)
+        self.tables["RentalPoints"] = construct_with_coords(root / self.data_paths["RentalPoints"],
+                                                            instaces.RentalPoint)
         self.tables["Restaurants"] = construct_with_coords(root / self.data_paths["Restaurants"], instaces.Restaraunt)
-        self.tables["Restaurants"].sort()
-        self.tables["TransportStops"] = construct_with_coords(root / self.data_paths["RentalPoints"], instaces.TransportStop)
+        self.tables["Restaurants"].sort(key=cmp_to_key(instaces.compare_restaurants), reverse=True)
+        self.tables["TransportStops"] = construct_with_coords(root / self.data_paths["RentalPoints"],
+                                                              instaces.TransportStop)
 
     def get_all_entities(
             self, coords: instaces.Coordinates, radius: instaces.Distance
@@ -65,8 +65,7 @@ class DatabaseApi:
         restaurants = [value for value in close_obj if type(value) == instaces.Restaraunt]
         stops = [value for value in close_obj if type(value) == instaces.TransportStop]
         rent_points = [value for value in close_obj if type(value) == instaces.RentalPoint]
-        #   TODO: res = evaluate_area(radius, restaurants, stops, rent_points)
-        return instaces.LocationDescription()
+        return instaces.LocationDescription(score=self.area_score(radius, stops, restaurants, rent_points))
 
     def evaluate_regions(self, rect: Rectangle):
         all_regions: t.List[t.Tuple[instaces.Coordinates, instaces.LocationDescription]] = []
@@ -85,7 +84,7 @@ class DatabaseApi:
                 tmp = self.evaluate_region(instaces.Coordinates(*cur), radius)
                 all_regions.append((instaces.Coordinates(*cur), tmp))
 
-        all_regions.sort(key=lambda x: x[1].score)
+        all_regions.sort(key=lambda x: x[1].score, reverse=True)
         return all_regions
 
     def get_best_locations(
@@ -115,10 +114,9 @@ class DatabaseApi:
     ) -> t.List[t.Tuple[instaces.Entity, instaces.EntityDescription]]:
         close_obj = self.get_all_entities(coords, radius)
         if preferences.entity_type == instaces.EntityType.RESTARAUNT:
-            restaurants = [(value, instaces.EntityDescription()) for value in close_obj if
+            restaurants = [value for value in close_obj if
                            type(value) == instaces.Restaraunt]
-            restaurants.sort()
-            return restaurants[:n_max_results]
+            return [(i, instaces.EntityDescription(i.descr(), i.rank())) for i in restaurants][:n_max_results]
         elif preferences.entity_type == instaces.EntityType.RENTAL_POINT:
             return [(value, instaces.EntityDescription()) for value in close_obj if
                     type(value) == instaces.TransportStop][:n_max_results]
@@ -126,3 +124,197 @@ class DatabaseApi:
             return [(value, instaces.EntityDescription()) for value in close_obj if
                     type(value) == instaces.RentalPoint][:n_max_results]
         return []
+
+    # FIRST Function - busses ±score calculator
+
+    def bus_rad_score(self, radius, busStopss):
+        bad = "Bad"
+        poor = "Poor"
+        decent = "Decent"
+        good = "Good"
+        excellent = "Excellent"
+        errorMsg = "Something went wrong"
+        stops = len(busStopss)
+        if radius <= 0.5:
+            if stops >= 3:
+                return excellent
+            elif stops >= 1:
+                return good
+            else:
+                return poor
+        elif radius <= 1:
+            if stops >= 6:
+                return excellent
+            elif stops >= 3:
+                return good
+            elif stops == 2:
+                return poor
+            else:
+                return bad
+        elif radius <= 3:
+            if stops >= 12:
+                return excellent
+            elif stops >= 8:
+                return good
+            elif stops >= 5:
+                return decent
+            elif stops >= 3:
+                return poor
+            else:
+                return bad
+        elif radius <= 5:
+            if stops >= 32:
+                return excellent
+            elif stops >= 20:
+                return good
+            elif stops >= 13:
+                return decent
+            elif stops >= 5:
+                return poor
+            else:
+                return bad
+        elif radius >= 5:
+            area = 3.14 * radius ** 2
+            ratio = stops / (area / 5.5)
+            if ratio >= 1.3:
+                return excellent
+            elif ratio >= 1.15:
+                return good
+            elif ratio >= 0.85:
+                return decent
+            elif ratio >= 0.75:
+                return poor
+            else:
+                return bad
+        else:
+            return errorMsg + " - busRadScore"
+
+    # FIRST END
+
+    # should return the restaurant's, given
+    # as a parameter, score _ Not Necessarily
+    # the scale from 1 to 10
+    # also requested by - KATE -
+
+    # FUN END
+
+    # CHECK RETURN VALUES, last things
+    # - they have a comment above -
+    # SECOND Function - restaurantTop 3 creator and ±score calculator
+    def restr_score(self, radius, restaurants):
+        sum = 0
+        for x in restaurants:
+            sum += x.rank()
+        return sum / (len(restaurants)+1)
+
+    # SECOND END
+
+    # THIRD Function - rental ±score calculator
+    def rental(self, radius, rentalPoints):
+        bad = "Bad"
+        poor = "Poor"
+        decent = "Decent"
+        good = "Good"
+        excellent = "Excellent"
+        errorMsg = "Something went wrong"
+        if (radius < 0.1):
+            return "Huh, well someone messed up - rental_fun"
+        # rentalPlaces = rentalPoints.count()
+        rentalPlaces = len(rentalPoints)
+
+        if (radius <= 2.5):
+            if (rentalPlaces >= 1):
+                return excellent
+        elif (radius <= 5):
+            if (rentalPlaces >= 3):
+                return excellent
+            elif (rentalPlaces >= 1):
+                return good
+            else:
+                return poor
+        elif (radius <= 10):
+            if (rentalPlaces >= 7):
+                return excellent
+            elif (rentalPlaces >= 5):
+                return good
+            elif (rentalPlaces >= 3):
+                return decent
+            elif (rentalPlaces >= 1):
+                return poor
+            else:
+                return bad
+        # I'd put a limit,
+        # just to be more foolProof
+        # this time I'll just do it
+        # like this tho - please put the
+        # right input :) -
+        else:
+            area = 3.14 * radius ** 2
+            area = area ** 0.5
+            # 5.5 is not random, it's just kinda close
+            # to giving the actual area of Berlin
+            # GEneralizing - BAD - Ik, with time we'll
+            # do it better '-_-'
+            ratio = rentalPlaces / (area / 5.5)
+            if (ratio >= 0.9):
+                return excellent
+            elif (ratio >= 0.7):
+                return good
+            elif (ratio >= 0.55):
+                return decent
+            elif (ratio >= 0.4):
+                return poor
+            else:
+                return bad
+
+    # THIRD END
+
+    # FOURTH and final Function
+    # It aims to generate a score for the Entire Area
+    def area_score(self, radius, busStops, restaurants, rentalPoints):
+        bad = "Bad"
+        poor = "Poor"
+        decent = "Decent"
+        good = "Good"
+        excellent = "Excellent"
+        errorMsg = "Something went wrong"
+        scorePublicTrasns = self.bus_rad_score(radius, busStops)
+        # I don't know if this will work - literally no clue
+        # scoreRestaurants should return a special defined class ?
+
+        resultRest = self.restr_score(radius, restaurants)
+        scoreRest = resultRest * 1.25
+        scoreRental = self.rental(radius, rentalPoints)
+
+        if (scorePublicTrasns == excellent):
+            scorePublicTrasns = 10
+        elif (scorePublicTrasns == good):
+            scorePublicTrasns = 8
+        elif (scorePublicTrasns == decent):
+            scorePublicTrasns = 6.25
+        elif (scorePublicTrasns == poor):
+            scorePublicTrasns = 5
+        elif (scorePublicTrasns == bad):
+            scorePublicTrasns = 4
+
+        if (scoreRental == excellent):
+            scoreRental = 10
+        elif (scoreRental == good):
+            scoreRental = 8
+        elif (scoreRental == decent):
+            scoreRental = 6.25
+        elif (scoreRental == poor):
+            scoreRental = 5
+        elif (scoreRental == bad):
+            scoreRental = 4
+
+        if (scoreRental == scorePublicTrasns and scorePublicTrasns == 4):
+            areaScore = scoreRest * 1 / 3
+        elif (scoreRental == scorePublicTrasns and scorePublicTrasns > 4):
+            areaScore = scoreRental * 2 / 3 + scoreRest * 1 / 3
+        else:
+            areaScore = (scorePublicTrasns + scorePublicTrasns + scoreRest) / 3
+
+        return areaScore
+
+    # FOURTH END
