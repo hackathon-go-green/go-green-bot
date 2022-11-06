7d73e94e60e301cc539deef7ae81a6695d776077
Glebanister <glebmar2001@gmail.com>
2022-11-06 10:26:32 +0100

    Update string names

--- interface/strings.py
+++ interface/strings.py
@@ -17,23 +17,23 @@ class BotString(enum.Enum):
     INVALID_RADIUS_TOO_BIG = (
         f"😔 Radius is too big, should be no more than {config.MAX_RADIUS} km"
     )
-    INVALID_ENTITY_KIND = """😔 <b>These</b> amazing places are not yet discovered by our team :(
+    INVALID_ENTITY_KIND = """😔 <b>These</b> amazing places are not yet discovered by our team 🙁
 
 Try out something from the given options!
 """
 
     DECISION_LOCATION = """📍 <b>Send location!</b>
 
-We will analyse this area and tell you which regions of this location are most sustainable!
+I will analyse this area and tell you which regions of this location are most sustainable!
 """
     DECISION_RADIUS = """🌐 <b> Send a radius </b> (in kilometers)
         
-Tell us in which radius do you consider traveling
+Tell me in which radius do you consider traveling
 """
 
     OVERALL_LOCATION = """📍 <b>Send any location!</b>
 
-In this location we will get all information about sustainability of restaurants, grocery shops, public transport stops and bike or scooter renting services
+In this location I will gather all information about sustainability of restaurants, grocery shops, public transport stops and bike or scooter renting services
 """
     OVERALL_RADIUS = """🌐 <b> Send a radius </b> (in kilometers)
         
@@ -42,7 +42,7 @@ The radius of the area you want a detailed report on
 
     INPLACE_LOCATION = """📍 <b>Send location to explore!</b>
 
-We will share with you the most sustainable places: farmer markets, cafes which are using local production and so on
+I will share with you the most sustainable places: farmer markets, cafes which are using local production and so on
 """
     INPLACE_RADIUS = """🌐 <b> Send a radius </b> (in kilometers)
         
@@ -50,7 +50,7 @@ Radius in which you want to find new places
 """
     INPLACE_ENTITY_KIND = """🧭 <b> Select specific type of place </b>
 
-Our ✨ <i>sustainability</i> ✨ faries 🧚 will voluntary select the best places of that kind!
+My ✨ <i>sustainability</i> ✨ faries 🧚 will voluntary select the best places of that kind!
 """
 
     SHARE_LOCATION = "📍 Share Current Location"
@@ -60,37 +60,37 @@ Our ✨ <i>sustainability</i> ✨ faries 🧚 will voluntary select the best pla
 This is a [some description]
 
 
-🧭 /decide - Helps you choose the most sustainable region to travel or to live
+🧭 /decide - Helps you choose the most sustainable region to travel to or to live in
 
-🍀 /overall - Get to know some location in terms of sustainability
+🍀 /overall - Find out how sustainable your whereabouts and surroundings are
 
-🏃🏾‍♀️ /inplace - Learn about the places to go out that care most about the environment near a given location
+🏃🏾‍♀️ /inplace - Feel like going out? Want to find the best and the nearest option?
 """
 
-    HELP = """🧭 /decide - Helps you choose the most sustainable region to travel or to live
+    HELP = """🧭 /decide - Helps you choose the most sustainable region to travel to or to live in
 
-🍀 /overall - Get to know some location in terms of sustainability
+🍀 /overall - Find out how sustainable your whereabouts and surroundings are
 
-🏃🏾‍♀️ /inplace - Learn about the places to go out that care most about the environment near a given location
+🏃🏾‍♀️ /inplace - Feel like going out? Want to find the best and the nearest option?
 """
 
     TOP_LOCATION = """<b>Choice 🍃 {}</b>
 <i>Score: {}/10</i>
-<b>***</b>
+<b>*</b>
 
 {}
 """
 
     REGION_DESCRIPTION = """🍃
 <i>Score: {}/10</i>
-<b>***</b>
+<b>*</b>
 
 {}
 """
 
     TOP_ENTITY = """<b>🍃 {}</b>
 <i>Score: {}/10</i>
-<b>***</b>
+<b>*</b>
 
 <b>Summary:</b>
 {}
@@ -99,7 +99,7 @@ This is a [some description]
 """
 
     ENTITY_DESC_VEGAN = "🥗 Vegan"
-    ENTITY_DESC_VEGETERIAN = "🥛🥚 Vegeterian"
+    ENTITY_DESC_VEGETERIAN = "🥛🥚 Vegetarian"
     ENTITY_DESC_VEG_OPTIONS = "🐣 Has vegan options"
     ENTITY_DESC_HAS_VEG_OPTIONS = "Uses local products (bio)"
     ENTITY_DESC_NO_VEG_OPTIONS = "Does not use local products (not bio)"
